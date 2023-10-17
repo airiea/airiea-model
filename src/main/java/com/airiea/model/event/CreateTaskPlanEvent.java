da4abd56ae273a14aebb4cccd04e3cf4b729a1e8
yifan lu <luyifane@amazon.com>
2023-10-16 23:24:05 -0400

    biu

--- src/main/java/com/airiea/model/event/CreateTaskPlanEvent.java
+++ src/main/java/com/airiea/model/event/CreateTaskPlanEvent.java
@@ -23,6 +23,18 @@ public class CreateTaskPlanEvent {
     @JsonProperty("entity_ids")
     List<String> entityIds;
 
-    @JsonProperty("agent_name")
-    String agentName;
+    @JsonProperty("agent_name_one")
+    String agentNameOne;
+
+    @JsonProperty("agent_name_two")
+    String agentNameTwo;
+
+    @JsonProperty("agent_name_three")
+    String agentNameThree;
+
+    @JsonProperty("agent_name_four")
+    String agentNameFour;
+
+    @JsonProperty("agent_name_five")
+    String agentNameFive;
 }
